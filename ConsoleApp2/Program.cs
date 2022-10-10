3d4f44751c661cf42727d075789ed6f61c33feb1
Hayk-1996 <hayk98837073@gmail.com>
2022-10-10 09:11:09 -0800

    new

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,9 +7,9 @@
         string insertString = Console.ReadLine();
         for (int i = 0; i < insertString.Length; i++)
             arr[insertString[i]]++;
-        
+
         int max = arr[0];
-        
+
         for (int i = 0; i < 400; i++)
             if (arr[i] >= max)
                 max = arr[i];
